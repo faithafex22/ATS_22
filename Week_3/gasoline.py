b70850870b19e88ae88520d43d7df0c7d6f3ce17
Faith Odunayo Adeosun <fadeosun@afexnigeria.com>
2022-06-27 15:34:33 +0100

    gasoline.py updated

--- Week_3/gasoline.py
+++ Week_3/gasoline.py
@@ -1,19 +1,19 @@
 def avetotal():
-
     gallons = []
     miles = []
     while True:
         m = float(input("Enter the number 0f miles driven: "))
         g = float(input("Enter the amount of gallons used: "))
-        if m == 1 and g == -1:
-            print ("not possible")
+        if m == -1 or g == -1:
+            print("This is not possible")
             break
+        print(f"The miles per gallon for this tank was {m/g}")
         miles.append(m) 
         gallons.append(g)
-        print(miles)
-        print(gallons)
-        average = sum(miles)/sum(gallons) 
-        print(average)
+        
+        
+    average = sum(miles)/sum(gallons) 
+    print(f"The overall aaverage miles/garlons was {average}")
         
     
 avetotal() 
